641720eef749285ec02f1f1726a02c41244cb71f
Nishka Olhyan <nishkavidip02@gmail.com>
2026-04-05 17:18:30 +0530

    Updated Assignment1 file

--- Assignment1.py
+++ Assignment1.py
@@ -1,4 +1,7 @@
-# Q1. Write a program that asks the user for their name and age, then prints a sentence like- "Hello Nishka, you are 21 years old."
-name=input("enter name-")
-age=int(input("enter age"))
-print("Hello",name,"you are",age,"years old.")
+# Q2. Take two numbers as input from the user and print their-sum,difference,product,and quotient
+num1=int(input("enter number 1-"))
+num2=int(input("enter number 2-"))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
