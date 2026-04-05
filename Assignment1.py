efdf7766873aed62833753ad9465ef0fb3f3ec2e
Nishka Olhyan <nishkavidip02@gmail.com>
2026-04-05 16:49:12 +0530

    first commit

---
+++ Assignment1.py
@@ -0,0 +1,4 @@
+# Q1. Write a program that asks the user for their name and age, then prints a sentence like- "Hello Nishka, you are 21 years old."
+name=input("enter name-")
+age=int(input("enter age"))
+print("Hello",name,"you are",age,"years old.")
---
+++ python.py
@@ -0,0 +1,6 @@
+U=input("Enter username-")
+P=input("Enter password-")
+if (U=="user" and P=="pass"):
+    print("Successful")
+else:
+    print("invalid")
