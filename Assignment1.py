1ecb860455b51d99b79d4af96319a909ba75e91f
Nishka Olhyan <nishkavidip02@gmail.com>
2026-04-06 10:12:31 +0530

    Updated Assignment1 file

--- Assignment1.py
+++ Assignment1.py
@@ -1,7 +1,6 @@
-'''Q6. Write a program to swap values of two numbers entered by the user.'''
-num1=int(input("print number1-"))
-num2=int(input("print number2-"))
-swap=num1
-num1=num2
-num2=swap
-print(num1,num2)
+'''Q7. Ask the user for a temperature in Celsius (string input).Convert it to float then calculate and print temperature in Fahrenheit. Conversion Formula: Fahrenheit Temp=[C∗(9/5)]+32
+Here C= Celsius Temperature'''
+temp=input("enter temperature in Celsius-")
+C_temp=float(temp)
+F_temp=(C_temp*(9/5))+32
+print("Celsius temperature converted into Fahrenheit temperature is-",F_temp,"F")
