3a14cf43c1b734ebddf94f0e89993a7c5a3a7147
Nishka Olhyan <nishkavidip02@gmail.com>
2026-04-08 11:02:07 +0530

    Updated python.py file

--- python.py
+++ python.py
@@ -1,6 +1,14 @@
-U=input("Enter username-")
-P=input("Enter password-")
-if (U=="user" and P=="pass"):
-    print("Successful")
-else:
-    print("invalid")
+class student:
+    
+    def __init__(self,name,cgpa):
+        self.name=name
+        self.cgpa=cgpa
+
+
+stu1=student("Nishka",9.1)
+stu2=student("Ravi",7.5)
+
+print(f"{stu1.name} has more marks than {stu2.name} and {stu2.name} loves {stu1.name}")
+
+
+
